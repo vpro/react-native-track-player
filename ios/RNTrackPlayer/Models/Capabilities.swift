f9d97eeea4eae34cdc9f6aa033bd4ed29f5326e7
frenkie <frank@frankbosma.nl>
2018-02-20 17:54:08 +0100

    Merge remote-tracking branch 'upstream/dev' into dev
    # Conflicts:
    #	ios/RNTrackPlayer/RNTrackPlayer.swift

--- ios/RNTrackPlayer/Models/Capabilities.swift
+++ ios/RNTrackPlayer/Models/Capabilities.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 enum Capability: String {
-    case play, pause, stop, next, previous, skipForward, skipBackward
+    case play, pause, stop, next, previous, jumpForward, jumpBackward
 }
