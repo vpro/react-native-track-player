a639daa2cd37571129dbafa9b8ba6e4396bc5eb1
frenkie <frank@frankbosma.nl>
2018-08-03 15:57:24 +0200

    TREES-478 - Testing merger of https://github.com/react-native-kit/react-native-track-player/pull/177/files

--- android/src/main/java/guichaguri/trackplayer/player/Playback.java
+++ android/src/main/java/guichaguri/trackplayer/player/Playback.java
@@ -233,12 +233,12 @@ public abstract class Playback {
 
         load(next, callback);
 
-        if(Utils.isPaused(oldState)) {
-            Log.d(Utils.TAG, "Pausing next track...");
-            pause();
-        } else {
+        if(Utils.isPlaying(oldState)) {
             Log.d(Utils.TAG, "Playing next track...");
             play();
+        } else if(Utils.isPaused(oldState)) {
+            Log.d(Utils.TAG, "Pausing next track...");
+            pause();
         }
 
         manager.onTrackUpdate(previous, position, next, true);
